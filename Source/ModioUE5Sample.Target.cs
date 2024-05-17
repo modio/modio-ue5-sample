3b1dc8a8687eeda202cb717c93fceac5fc012cb8
nferrar-modio <nicholas.ferrar@mod.io>
2024-05-17 11:16:26 +1000

    build and target guards, clean up redirectors
    while the redirectors aren't necessarily related to 5.4, they were muddying build logs.

--- Source/ModioUE5Sample.Target.cs
+++ Source/ModioUE5Sample.Target.cs
@@ -8,8 +8,15 @@ public class ModioUE5SampleTarget : TargetRules
 	public ModioUE5SampleTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
 
-		ExtraModuleNames.AddRange( new string[] { "ModioUE5Sample" } );
+#if UE_5_3_OR_LATER
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+#else
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+#endif
+
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+
+        ExtraModuleNames.AddRange( new string[] { "ModioUE5Sample" } );
 	}
 }
